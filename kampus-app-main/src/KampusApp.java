93284a6c8637e00f9082cd292d283b7bf9d279c2
chikadida <148084816+chikadida@users.noreply.github.com>
2023-10-30 21:24:01 +0700

    Update KampusApp.java

--- kampus-app-main/src/KampusApp.java
+++ kampus-app-main/src/KampusApp.java
@@ -2,9 +2,10 @@ import java.util.Scanner;
 
 import model.Mahasiswa;
 import repository.Database;
+import model.Prodi;
 
 public class KampusApp {
-    public static void main(String[] arg) {
+    public static void main(String[] args) {
         KampusApp kampusApp = new KampusApp();
         kampusApp.showMenu();
     }
@@ -25,7 +26,11 @@ public class KampusApp {
         System.out.println("2 -> Ubah Data Mahasiswa");
         System.out.println("3 -> Hapus Data Mahasiswa");
         System.out.println("4 -> Cari Data Mahasiswa");
-        System.out.println("0 -> Keluar Aplikasi");
+        System.out.println("5 -> Tambah ID Prodi ");
+        System.out.println("6 -> Ubah ID Prodi ");
+        System.out.println("7 -> Hapus ID Prodi ");
+        System.out.println("8 -> Cari ID Prodi");
+        System.out.println("\n0 -> Keluar Aplikasi");
         System.out.print("Silahkan masukan menu yang dipilih: ");
         int menuYangDipilih = scanner.nextInt();
         scanner.nextLine();
@@ -42,6 +47,18 @@ public class KampusApp {
             case 4:
             menuCari();
             break;
+            case 5:
+            menuTambahIDProdi();
+            break;
+            case 6:
+            menuUbahIDProdi();
+            break;
+            case 7:
+            menuHapusIDProdi();
+            break;
+            case 8:
+            menuCariIDProdi();
+            break;
             default: {
                 System.out.print("* Terimakasih sudah menggunakan Aplikasi Kampus *");
                 System.exit(0);
@@ -57,8 +74,17 @@ public class KampusApp {
         String nim = scanner.nextLine();
         System.out.print("Masukan Nama: ");
         String nama = scanner.nextLine();
-        Mahasiswa mahasiswa = new (nim, nama);
-        db.mahasiswaTbl.create(mahasiswa);
+        System.out.print("Masukan ID Prodi: ");
+        String IDProdi = scanner.nextLine();
+        Prodi prodi = db.prodiTbl.getProdiById(IDProdi);
+        if (prodi == null) {
+            System.out.println("Prodi dengan ID " + IDProdi + " tidak dapat ditemukan.");
+        } else {
+            Mahasiswa mahasiswa = new Mahasiswa(nim, nama, IDProdi);
+            db.mahasiswaTbl.create(mahasiswa);
+            System.out.println(nama + " Bergabung di: " + prodi.getNamaProdi());
+        }
+
         System.out.println("Tekan Enter untuk melanjutkan...");
         scanner.nextLine();
         showMenu();
@@ -72,24 +98,37 @@ public class KampusApp {
         String nimBaru = scanner.nextLine();
         System.out.print("Masukan Nama: ");
         String nama = scanner.nextLine();
-        Mahasiswa mahasiswa = new Mahasiswa(nimBaru, nama);
+        System.out.println("Masukan ID Prodi: ");
+        String prodi = scanner.nextLine();
+        Mahasiswa mahasiswa = new Mahasiswa(nimBaru, nama, prodi);
         db.mahasiswaTbl.update(nimLama, mahasiswa);
         System.out.println("Tekan Enter untuk melanjutkan...");
         scanner.nextLine();
         showMenu();
     }
 
-    public void menuCari(){
+    public void menuCari() {
         System.out.println("----- Menu Cari Data Mahasiswa -----");
-        System.out.print("Masukan NIM: ");
+        System.out.print("Masukkan NIM: ");
         String nim = scanner.nextLine();
         Mahasiswa mahasiswa = db.mahasiswaTbl.getMahasiswaByNim(nim);
-        if(mahasiswa == null) {
+    
+        if (mahasiswa == null) {
             System.out.println("* NIM tidak ditemukan *");
         } else {
             System.out.println("* NIM: " + mahasiswa.getNim() + " *");
             System.out.println("* Nama: " + mahasiswa.getNama() + " *");
+    
+            String IDProdi = mahasiswa.getProdi(); // Mengambil ID Prodi dari Mahasiswa
+            Prodi prodi = db.prodiTbl.getProdiById(IDProdi);
+    
+            if (prodi == null) {
+                System.out.println("* ID Prodi tidak ditemukan *");
+            } else {
+                System.out.println("* Nama Prodi: " + prodi.getNamaProdi() + " *");
+            }
         }
+    
         System.out.println("Tekan Enter untuk melanjutkan...");
         scanner.nextLine();
         showMenu();
@@ -104,4 +143,59 @@ public class KampusApp {
         scanner.nextLine();
         showMenu();
     }
+
+    public void menuTambahIDProdi() {
+        System.out.println("----- Menu Tambah ID Prodi -----");
+        System.out.print("Masukan ID: ");
+        String id = scanner.nextLine();
+        System.out.print("Masukan Nama Prodi: ");
+        String nama = scanner.nextLine();
+        Prodi prodi = new Prodi(id, nama);
+        db.prodiTbl.create(prodi);
+        System.out.println("Tekan Enter untuk melanjutkan...");
+        scanner.nextLine();
+        showMenu();
+    }
+
+    public void menuUbahIDProdi() {
+        System.out.println("----- Menu Ubah Data Prodi -----");
+        System.out.print("Masukan ID Sebelumnya: ");
+        String IDlama = scanner.nextLine();
+        System.out.print("Masukan ID: ");
+        String IDbaru = scanner.nextLine();
+        System.out.print("Masukan Nama Prodi: ");
+        String nama = scanner.nextLine();
+        Prodi prodi = new Prodi (IDbaru, nama);
+        db.prodiTbl.update (IDlama, prodi);
+        System.out.println("Tekan Enter untuk melanjutkan...");
+        scanner.nextLine();
+        showMenu();
+    }
+    
+    public void menuCariIDProdi() {
+        System.out.println("----- Menu Cari Data Prodi -----");
+        System.out.print("Masukan ID: ");
+        String id = scanner.nextLine();
+        Prodi prodi = db.prodiTbl.getProdiById(id);
+        if (prodi == null) {
+            System.out.println("* ID Prodi tidak ditemukan *");
+        } else {
+            System.out.println("* ID Prodi: " + prodi.getId() + " *");
+            System.out.println("* Nama Prodi: " + prodi.getNamaProdi() + " *");
+        }
+        System.out.println("Tekan Enter untuk melanjutkan...");
+        scanner.nextLine();
+        showMenu();
+    }
+    
+    public void menuHapusIDProdi() {
+        System.out.println("----- Menu Hapus Data Prodi -----");
+        System.out.print("Masukan ID: ");
+        String id = scanner.nextLine();
+        db.prodiTbl.delete(id);
+        System.out.println("Tekan Enter untuk melanjutkan...");
+        scanner.nextLine();
+        showMenu();
+    }
+    
 }
